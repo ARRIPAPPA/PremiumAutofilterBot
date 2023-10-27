34e0c3e0020939e2ab0efde323d2f2a1d5a71e64
rarakukka <147248841+rarakukka@users.noreply.github.com>
2023-10-27 06:24:05 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -41,7 +41,7 @@ async def save_group(bot, message):
                     InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                     InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                  ],[
-                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/ShinobiUnder")
+                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Blaster_Originals")
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -89,8 +89,8 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support Group', url="https://t.me/+P-zPGy3Gjy1kOWQ9"),
-            InlineKeyboardButton('Owner', url="https://t.me/ShinobiUnder")
+            InlineKeyboardButton('Support Group', url="https://t.me/Blaster_Movie_Request"),
+            InlineKeyboardButton('Owner', url="https://t.me/Blaster_Originals")
         ],[
             InlineKeyboardButton('Use Me Here', url=f'https://t.me/{SUPPORT_CHAT}')
         ]]
